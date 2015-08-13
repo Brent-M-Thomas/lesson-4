081f08d1fc4e08af69a3c4755ce4490cb31bf7ba
Brent Thomas <brentmt24@gmail.com>
2015-08-13 11:50:54 -0500

    exercise b done

---
+++ app/routes/blog/show.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model:function(params) {
+    var posts = this.modelFor('blog');
+    return posts.find(function(post) {
+      return post._id === params.id;
+    });
+  }
+});
